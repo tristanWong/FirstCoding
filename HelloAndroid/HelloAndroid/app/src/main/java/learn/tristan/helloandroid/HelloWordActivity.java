e18f09db3de4fcf0488bda90397c00357b9aefa8
tristanWong <wengtianliang@163.com>
2016-04-08 09:39:52 +0800

    FirstCode CODING

---
+++ HelloAndroid/HelloAndroid/app/src/main/java/learn/tristan/helloandroid/HelloWordActivity.java
@@ -0,0 +1,17 @@
+package learn.tristan.helloandroid;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class HelloWordActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_word);
+    }
+
+
+}
