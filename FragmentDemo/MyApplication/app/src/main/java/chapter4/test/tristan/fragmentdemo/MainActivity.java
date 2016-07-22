8b14702c59e8e542bb9bf2e77faba90ef2c71f08
tristanWong <wengtianliang@163.com>
2016-07-22 23:48:54 +0800

    fragment add

--- FragmentDemo/MyApplication/app/src/main/java/chapter4/test/tristan/fragmentdemo/MainActivity.java
+++ FragmentDemo/MyApplication/app/src/main/java/chapter4/test/tristan/fragmentdemo/MainActivity.java
@@ -6,10 +6,12 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import layout.AnotherFragment;
+import layout.RightFragment;
 
 public class MainActivity extends Activity implements View.OnClickListener{
     Button btn = null;
@@ -17,25 +19,45 @@ public class MainActivity extends Activity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initView();
+
+    }
+
+    private void initView() {
         btn = (Button) findViewById(R.id.button);
         btn.setOnClickListener(this);
+
+        initRightFragment();
+
+    }
+
+    private void initRightFragment() {
+        RightFragment rightFragment = new RightFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.right_fragment,rightFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.button:
-//                showAnotherFragment();
+                showAnotherFragment();
                 break;
         }
     }
 
-//    private void showAnotherFragment() {
-//        AnotherFragment anotherFragment = new AnotherFragment();
-//        FragmentManager fragmentManager = getFragmentManager();
-//        FragmentTransaction transaction = fragmentManager.beginTransaction();
-//        transaction.replace(R.id.right_layout,anotherFragment);
-//        transaction.addToBackStack(null);
-//        transaction.commit();
-//    }
+    private void showAnotherFragment() {
+        Log.d("FRAGMENT","Hello Another Fragment");
+        AnotherFragment anotherFragment = new AnotherFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_fragment,anotherFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
